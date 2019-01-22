b9c798b1012d9d2e17c2248bbbc5479907a9596a
leecode <2804581329@qq.com>
2019-01-22 23:54:51 +0800

    just finished half.

--- app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice05ComposeShaderView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice05ComposeShaderView.java
@@ -1,11 +1,20 @@
 package com.hencoder.hencoderpracticedraw2.practice;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.BitmapShader;
 import android.graphics.Canvas;
+import android.graphics.ComposeShader;
 import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.Shader;
+import android.service.quicksettings.Tile;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
+
+import com.hencoder.hencoderpracticedraw2.R;
 
 public class Practice05ComposeShaderView extends View {
     Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
@@ -28,6 +37,13 @@ public class Practice05ComposeShaderView extends View {
         // 用 Paint.setShader(shader) 设置一个 ComposeShader
         // Shader 1: BitmapShader 图片：R.drawable.batman
         // Shader 2: BitmapShader 图片：R.drawable.batman_logo
+        Bitmap bt1 = BitmapFactory.decodeResource(getResources(), R.drawable.batman);
+        Bitmap bt2 = BitmapFactory.decodeResource(getResources(), R.drawable.batman_logo);
+        Shader sd1 = new BitmapShader(bt1, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP);
+        Shader sd2 = new BitmapShader(bt2, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP);
+        Shader comer = new ComposeShader(sd1, sd2, PorterDuff.Mode.DST_IN);
+        paint.setShader(comer);
+
     }
 
     @Override
